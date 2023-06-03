0aeb5589b7dfc8edd4e3a9d96fda1bdde50c99f9
Kenny Pee <114894332+strawhatduckk@users.noreply.github.com>
2023-06-03 22:27:22 +0800

    Handle GET request from TIH endpoint

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-
-	// "os"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
+var TIH_URL = "https://api.stb.gov.sg/content/food-beverages/v2/search?searchType=keyword"
+
 func main() {
-	fmt.Println("Hello Worwwwld!")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// TIH_API_KEY := os.Getenv("TIH_API_KEY")
+	TIH_API_KEY := os.Getenv("TIH_API_KEY")
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
@@ -27,9 +28,31 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write([]byte(TIH_API_KEY))
-		w.Write([]byte("something"))
+		searchValue := r.URL.Query().Get("searchValue")
+		client := &http.Client{}
+
+		reqURL := fmt.Sprintf("%s%s", TIH_URL, searchValue)
+		req, err := http.NewRequest("GET", reqURL, nil)
+		if err != nil {
+			http.Error(w, "Error creating request", http.StatusInternalServerError)
+			return
+		}
+
+		req.Header.Add("X-API-Key", TIH_API_KEY)
+		resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, "Error making request", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Error reading response", http.StatusInternalServerError)
+			return
+		}
+		w.Write(body)
 	})
 
-	http.ListenAndServe(":3333", r)
+	log.Fatal(http.ListenAndServe(":3333", r))
 }
